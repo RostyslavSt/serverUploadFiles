6e8167fb9792316354732296bf30b573ab5ecf59
Shtets <rostyslav.shtets@sigmaukraine.com>
2018-09-07 09:54:56 +0300

    refactoring

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-var formidable = require('formidable');
-var fs = require('fs');
+let express = require('express');
+let app = express();
+let path = require('path');
+let formidable = require('formidable');
+let fs = require('fs');
 
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -21,48 +21,43 @@ app.get('/test', function (req, res) {
 
 // app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'views/index.html'));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-app.post('/upload', function(req, res){
+app.post('/upload', function (req, res) {
 
-  // create an incoming form object
-  var form = new formidable.IncomingForm();
+    // create an incoming form object
+    let form = new formidable.IncomingForm();
 
-  // specify that we want to allow the user to upload multiple files in a single request
-  form.multiples = true;
+    // parse the incoming request containing the form data
+    form.parse(req);
 
-  // store all uploads in the /uploads directory
-  form.uploadDir = path.join(__dirname, '/uploads');
+    // specify that we want to allow the user to upload multiple files in a single request
+    form.multiples = true;
 
-  // every time a file has been uploaded successfully,
-  // rename it to it's orignal name
-  form.on('file', function(field, file) {
-    fs.rename(file.path, path.join(form.uploadDir, file.name));
-  });
+    // store all uploads in the /uploads directory
+    form.uploadDir = path.join(__dirname, '/uploads');
 
-  // log any errors that occur
-  form.on('error', function(err) {
-    console.log('An error has occured: \n' + err);
-  });
+    // every time a file has been uploaded successfully,
+    // rename it to it's orignal name
+    form.on('file', function (field, file) {
+        fs.rename(file.path, path.join(form.uploadDir, file.name));
+    });
 
-  // once all the files have been uploaded, send a response to the client
-  form.on('end', function() {
-    res.end('success');
-  });
+    // log any errors that occur
+    form.on('error', function (err) {
+        console.log('An error has occured: \n' + err);
+    });
 
-  // parse the incoming request containing the form data
-  form.parse(req);
+    // once all the files have been uploaded, send a response to the client
+    form.on('end', function () {
+        res.end('success');
+    });
 
+    
 });
 
 app.listen(3002, function (a) {
     console.log("Listening to port 3002");
 });
-
-
-
-
-
-
